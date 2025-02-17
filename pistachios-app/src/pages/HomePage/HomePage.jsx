3cb2bf7f9b1a81e2e6db57b813a2099504f29090
joshlawlor <joshlawlor1@gmail.com>
2025-02-17 18:31:37 -0500

    Basic homepage route + page setup

---
+++ pistachios-app/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./HomePage.css";
+
+
+function HomePage() {
+
+  return (
+    <div >
+        
+<div>
+    <h1>HomePage</h1>
+
+</div>
+    </div>
+  );
+}
+
+export default HomePage;
