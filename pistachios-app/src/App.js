2b16fc63006b7907f2404d07c503ae1c73c51f86
joshlawlor <joshlawlor1@gmail.com>
2025-02-17 19:36:55 -0500

    Subsidary Pages + Routes added.

--- pistachios-app/src/App.js
+++ pistachios-app/src/App.js
@@ -5,6 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes, Link } from "react-router-dom";
 
 import Home from "./pages/HomePage/HomePage";
+import Events from "./pages/EventsPage/EventsPage";
+import Menu from "./pages/MenuPage/MenuPage";
+import Blog from "./pages/BlogPage/BlogPage";
+import Contact from "./pages/ContactPage/ContactPage";
+
 
 function App() {
   return (
@@ -12,16 +17,46 @@ function App() {
       <div className="App-Nav">
         <div className="App-Title-Container">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>PISTACHIOS
-          </h1>
+          <h1>PISTACHIOS</h1>
         </div>
-        <div className="navLinks">
-          <Link className="navLinks" to="/">HOME</Link>
+        <div className="navLinks-Container">
+          <div>
+            {" "}
+            <Link className="navLinks" to="/">
+              HOME
+            </Link>
+          </div>
+          <div>
+            {" "}
+            <Link className="navLinks" to="/events">
+              EVENTS
+            </Link>
+          </div>
+          <div>
+          <Link className="navLinks" to="/menu">
+            MENU
+          </Link>
+          </div>
+          <div>
+          <Link className="navLinks" to="/blog">
+            BLOG
+          </Link>
+          </div>
+          
+          <div>
+          <Link className="navLinks" to="/contact">
+            CONTACT
+          </Link>
+          </div>
         </div>
       </div>
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/blog" element={<Blog />} />
+
       </Routes>
     </div>
   );
---
+++ pistachios-app/src/pages/EventsPage/EventsPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./EventsPage.css";
+
+
+
+
+function EventsPage(){
+    return (
+
+        <div className="eventsMain">
+            <h1>Events Page</h1>
+        </div>
+    )
+}
+
+export default EventsPage;
